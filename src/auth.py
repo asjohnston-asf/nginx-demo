97a159fc32f52d7d87bb52fc8decf13313e31be6
Andrew Johnston <asjohnston@alaska.edu>
2019-12-20 15:51:21 -0900

    log username in access log

--- src/auth.py
+++ src/auth.py
@@ -19,15 +19,17 @@ def application(env, start_response):
         start_response('401 Unauthorized', [])
         return([])
 
+    user_id = payload[env['JWT_USER_FIELD']]
+    header = ('user_id', user_id)
+
     if 'JWT_GROUP_NAME' in env and 'JWT_APP_UID' in env:
         found = False
         for group in payload.get('urs-groups', []):
             if group.get('name') == env['JWT_GROUP_NAME'] and group.get('app_uid') == env['JWT_APP_UID']:
                 found = True
         if not found:
-            start_response('403 Forbidden', [])
+            start_response('403 Forbidden', [header])
             return([])
 
-    user_id = payload[env['JWT_USER_FIELD']].encode()
-    start_response('200 OK', [('Content-Type','text/html')])
-    return [user_id]
+    start_response('200 OK', [header])
+    return([])
